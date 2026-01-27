394b7bc590a019107745d00f4bf20f68dce63791
Ondrej Páleník <palenond@eurowag.com>
2026-01-27 14:23:20 +0100

    downgrade to Python 3.11

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import sys
 
 if hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix):
     python_root = sys.base_prefix  # system Python
-    os.environ['TCL_LIBRARY'] = r"C:\Users\palenond\AppData\Local\Programs\Python\Python314\tcl\tcl8.6"
-    os.environ['TK_LIBRARY'] = r"C:\Users\palenond\AppData\Local\Programs\Python\Python314\tcl\tk8.6"
+    os.environ['TCL_LIBRARY'] = r"C:\Users\palenond\AppData\Local\Programs\Python\Python311\tcl\tcl8.6"
+    os.environ['TK_LIBRARY'] = r"C:\Users\palenond\AppData\Local\Programs\Python\Python311\tcl\tk8.6"
 
 import tkinter as tk
 
